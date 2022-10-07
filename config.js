92925172987f9473aa997ed3f113c78b08f61237
Salim Mahboubi <salim.mahboubi@share-now.com>
2022-10-07 09:41:54 +0200

    feat(ext-003): Add config file + change pause to break

---
+++ config.js
@@ -0,0 +1,9 @@
+// All values are in hours
+export default config = {
+    FIXED_STARTING_TIME : undefined,
+    EARLIEST_RANDOM_STARTING_TIME : 8,
+    LATEST_STARTING_TIME : 12,
+    START_TO_END_DURATION : 9,
+    START_TO_BREAK_DETLA : 4,
+    BREAK_DURATION : 1,
+}
--- fill-time-table.js
+++ fill-time-table.js
@@ -1,24 +1,26 @@
+import config from "./config";
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-
-
 startElements = document.querySelectorAll('[id^="start-input"]');
 endElements = document.querySelectorAll('[id^="end-input"]');
 
-startingHour = getRandomInt(8, 12)
-endingHour = startingHour + 9;
+startingHour = conig.FIXED_STARTING_TIME ?
+    config.FIXED_STARTING_TIME 
+    : getRandomInt(config.EARLIEST_RANDOM_STARTING_TIME, config.LATEST_STARTING_TIME)
+endingHour = startingHour + config.START_TO_END_DURATION;
 
-startPause = startingHour + 4;
-endPause = startPause + 1;
+startBreak = startingHour + config.START_TO_BREAK_DETLA;
+endBreak = startBreak + config.BREAK_DURATION;
 
 startElements[0].value = `${startingHour}:00`;
 
 endElements[0].value = `${endingHour}:00`;
 
-startElements[1].value = `${startPause}:00`;
+startElements[1].value = `${startBreak}:00`;
 
-endElements[1].value = `${endPause}:00`;
+endElements[1].value = `${endBreak}:00`;
